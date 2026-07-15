828b8e041a077a7d7ea5b137d8516f0b335a9c00
SHIN <abdoullah.zahid.abz@gmail.com>
2026-07-15 10:09:30 +0100

    ready for deploy

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/send',(req, res)=>{
+router.get('/send',(_, res)=>{
     res.send('Send messages endpoint');
 });
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.routes.js';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
+
+//ready for deployment
+if(process.env.NODE_ENV=='production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*",(_, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
